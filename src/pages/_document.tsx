8871c5cb6eb9bdfb919129724ca5f1f555a18b78
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-06-10 15:40:34 +0530

    :recycle: Added og meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,14 @@ class CustomDocument extends Document<ICustomDocumentProps> {
           />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#2E3A49" />
+          <meta property="og:image" content="next-assets/avatar.svg" />
+          <meta property="og:title" content="Sethuraman Srinivasan" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Sethuraman Srinivasan is a full-stack developer working in Bengaluru, India 🇮🇳"
+          />
+          <meta property="og:locale" content="en_IN" />
           <link rel="icon" href="/favicon.ico?v=4" />
           <link
             rel="apple-touch-icon"
