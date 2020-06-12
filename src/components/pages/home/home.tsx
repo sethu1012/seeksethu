2bb93e5372cfb2c080e8ce33614a0f4a23745976
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-06-12 07:56:24 +0530

    :recycle: Responsive tailwind classes added

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -5,10 +5,10 @@ function HomePageComponent() {
     <div className="min-h-screen bg-grey-300 flex flex-col items-center justify-center">
       <img
         src="/next-assets/avatar.svg"
-        className="w-48 rounded-full"
+        className="w-32 sm:w-40 md:lg-48 lg:w-56 xl:w-64 rounded-full"
         alt="Sethuraman Srinivasan"
       />
-      <h1 className="text-blue-600 text-6xl text-center">
+      <h1 className="text-blue-600 text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl text-center">
         Sethuraman Srinivasan
       </h1>
     </div>
